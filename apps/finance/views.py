bacb2c6a63ce12a5fd86228e261f49c546d3c777
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-02-28 12:09:18 +0330

    checkout template

--- apps/finance/views.py
+++ apps/finance/views.py
@@ -14,10 +14,25 @@ class PaymentCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
-        context['cart'] = self.request.user.carts.get(is_paid=False)
+        context['cart'] = self.request.user.carts.get(user=self.request.user, is_paid=False)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        cart = self.request.user.carts.get(user=self.request.user, is_paid=False)
+        # Payment.objects.create(user=user, cart=cart, is_paid=True, is_active=True)
+        method = self.request.POST.get('payment_method')
+        print(method)
+        print('done'*100)
+
+    def create(self, *args, **kwargs):
+        print('hre')
 
     def form_valid(self, form):
+        print(form.instance)
         form.instance.user = self.request.user
-        form.instance.cart_id = form.cleaned_data['cart_id']
+        form.instance.cart = self.request.user.carts.get(user=self.request.user, is_paid=False)
+        form.instance.is_paid = True
+        form.save(commit=False)
 
         return super().form_valid(form)
