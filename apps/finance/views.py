0779e51de223850260ac9a2bdc2ad40d32936824
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-02-28 11:11:30 +0330

    checkout create view get

--- apps/finance/views.py
+++ apps/finance/views.py
@@ -9,3 +9,11 @@ class PaymentCreateView(CreateView):
     fields = ('payment_method',)
     template_name = 'finance/checkout.html'
     success_url = reverse_lazy('finance:success')
+
+    def get_context_data(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['cart'] = self.request.user.carts.get(is_active=True)
+
+    def post(self, request, *args, **kwargs):
+        ...
